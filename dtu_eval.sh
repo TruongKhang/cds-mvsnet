3798d0e9e9945aed556493805164e1385f48186c
TruongKhang <truongkhang95@gmail.com>
2021-11-19 13:05:05 +0900

    update

--- dtu_eval.sh
+++ dtu_eval.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 TESTPATH="/mnt/sdb/khang/dtu_dataset/test"
 TESTLIST="lists/dtu/test.txt"
-CKPT_FILE="pretrained/cds_mvsnet.ckpt"
-python test.py --dataset dtu --batch_size 1 --testpath $TESTPATH --testlist $TESTLIST --resume $CKPT_FILE --outdir dtu_outputs --interval_scale 1.06 --num_view 5 --numdepth 192 --max_h 1152 --max_w 1536 --filter_method gipuma --disp_threshold 0.1 --num_consistent 2
+CKPT_FILE=$1
+OUTDIR=$2
+python test.py --dataset dtu --batch_size 1 --testpath $TESTPATH --testlist $TESTLIST --resume $CKPT_FILE --outdir $OUTDIR --interval_scale 1.06 --num_view 5 --numdepth 192 --max_h 832 --max_w 1152 --filter_method gipuma --disp_threshold 0.1 --num_consistent 2
