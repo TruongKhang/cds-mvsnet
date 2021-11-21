6ff10c4da290a3c459d0cb3033498e4f9eb985e9
TruongKhang <truongkhang95@gmail.com>
2021-11-21 19:18:16 +0900

    update

--- dtu_eval.sh
+++ dtu_eval.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-TESTPATH="/mnt/sdb/khang/dtu_dataset/test"
+TESTPATH=$1
 TESTLIST="lists/dtu/test.txt"
-CKPT_FILE=$1
-OUTDIR=$2
+CKPT_FILE=$2
+OUTDIR=$3
 python test.py --dataset dtu --batch_size 1 --testpath $TESTPATH --testlist $TESTLIST --resume $CKPT_FILE --outdir $OUTDIR --interval_scale 1.06 --num_view 5 --numdepth 192 --max_h 1152 --max_w 1536 --filter_method gipuma --disp_threshold 0.1 --num_consistent 2
--- tt_eval.sh
+++ tt_eval.sh
@@ -1,5 +1,5 @@
-TESTPATH="/mnt/sdb/khang/tanksandtemples/intermediate/"
-CKPT_FILE="pretrained/cds_mvsnet.ckpt"
+TESTPATH=$1
+CKPT_FILE=$2
 echo Family > lists/tanksandtemples/subset.txt && python test.py --dataset tt --batch_size 1 --testpath $TESTPATH --testlist lists/tanksandtemples/subset.txt --resume $CKPT_FILE --outdir tt_outputs --interval_scale 1.0 --num_view 10 --numdepth 256 --max_h 1088 --max_w 1920 --no_refinement --filter_method gipuma --prob_threshold 0.8,0.8,0.8 --disp_threshold 0.4 --num_consistent 5 && cp tt_outputs/Family/points_mvsnet/con*/final3d_model.ply tt_outputs/Family.ply
 echo Francis > lists/tanksandtemples/subset.txt && python test.py --dataset tt --batch_size 1 --testpath $TESTPATH --testlist lists/tanksandtemples/subset.txt --resume $CKPT_FILE --outdir tt_outputs --interval_scale 1.0 --num_view 10 --numdepth 256 --ndepths 64,32,8 --max_h 1088 --max_w 1920 --no_refinement --conf 0.8,0.8,0.8 --thres_disp 0.6 --thres_view 5 #&& mv tt_outputs/Francis/points_mvsnet/con*/final*.ply tt_outputs/Francis.ply
 echo Horse > lists/tanksandtemples/subset.txt && python test.py --dataset tt --batch_size 1 --testpath $TESTPATH --testlist lists/tanksandtemples/subset.txt --resume $CKPT_FILE --outdir tt_outputs --interval_scale 1.0 --num_view 10 --numdepth 256 --ndepths 64,32,8 --max_h 1088 --max_w 1920 --no_refinement --conf 0.7,0.7,0.7 --thres_view 4 --thres_disp 0.8
