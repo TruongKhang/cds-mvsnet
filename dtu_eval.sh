c64f3c0cb1a9aca3d68009e895b873b7473055b3
TruongKhang <truongkhang95@gmail.com>
2021-11-21 13:56:09 +0900

    update

--- dtu_eval.sh
+++ dtu_eval.sh
@@ -3,4 +3,4 @@ TESTPATH="/mnt/sdb/khang/dtu_dataset/test"
 TESTLIST="lists/dtu/test.txt"
 CKPT_FILE=$1
 OUTDIR=$2
-python test.py --dataset dtu --batch_size 1 --testpath $TESTPATH --testlist $TESTLIST --resume $CKPT_FILE --outdir $OUTDIR --interval_scale 1.06 --num_view 5 --numdepth 192 --max_h 832 --max_w 1152 --filter_method gipuma --disp_threshold 0.1 --num_consistent 2
+python test.py --dataset dtu --batch_size 1 --testpath $TESTPATH --testlist $TESTLIST --resume $CKPT_FILE --outdir $OUTDIR --interval_scale 1.06 --num_view 5 --numdepth 192 --max_h 1152 --max_w 1536 --filter_method gipuma --disp_threshold 0.1 --num_consistent 2
